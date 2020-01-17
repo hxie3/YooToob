22ac71f8bc37a8202f75fbb5563a0cf5d91d6072
Herman <hexie646>
2020-01-17 02:42:49 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,67 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
-User.create([{username: 'DemoUser123', password: 'DemoUser123'}])
-User.first.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+user = User.create(username: 'DemoUser123', password: 'DemoUser123')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+
+user = User.create(username: 'Shirrako', password: 'Shirrako')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "Dark Souls III Soundtrack OST - Soul of Cinder", description: "Dark Souls 3 Official OST", user_id: 2)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/darksouls.mp4")
+video.video.attach(io: file, filename: "ds3.mp4")
+
+user = User.create(username: 'Dewey Griffin', password: 'Dewey Griffin')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "Maple Story", description: "Maple Sit-tory dance", user_id: 3)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/maplestory.mp4")
+video.video.attach(io: file, filename: "maplestory.mp4")
+
+user = User.create(username: 'The Dodo', password: 'The Dodo')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "can I get a bowl of that chili", description: 'wake up cole', user_id: 4)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/can+i+get+a+bowl+of+that+chili.mp4")
+video.video.attach(io: file, filename: "can i get a bowl of that chili.mp4")
+
+user = User.create(username: 'IGN', password: 'IGN')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "Weathering With You Official Subbed Trailer", description: "Weathering With You follows Hodaka, a high school runaway alone in Tokyo. Not long after finding work as a writer for an occult magazine, he meets Hina, a girl who has the ability to clear away the seemingly neverending rain. Makoto Shinkai's (Your Name) next movie hits theaters in North America beginning January 17.", user_id: 6)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/Weathering+With+You+Official+Subbed+Trailer.mp4")
+video.video.attach(io: file, filename: "Weathering With You Official Subbed Trailer.mp4")
+
+user = User.create(username: 'stopplimertime', password: 'stopplimertime')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "can I get a bowl of that chili", description: 'wake up cole', user_id: 6)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/can+i+get+a+bowl+of+that+chili.mp4")
+video.video.attach(io: file, filename: "can i get a bowl of that chili.mp4")
+
+user = User.create(username: 'Gone Viral', password: 'Gone Viral')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "It's just a frog...", description: 'No Description', user_id: 7)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/It's+just+a+frog....mp4")
+video.video.attach(io: file, filename: "It's just a frog.mp4")
+
+user = User.create(username: 'Jack Stauber', password: 'Jack Stauber')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: 'Hot dogs', description: 'I love you Dougie', user_id: 8)
+file = open('https://yootoob-seeds.s3-us-west-1.amazonaws.com/hot+dogs.mp4')
+video.video.attach(io: file, filename: 'hot_dogs.mp4')
+
+user = User.create(username: 'Scooty Booty', password: 'Scooty Booty')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: 'You may enter', description: 'That poor kid', user_id: 9)
+file = open('https://yootoob-seeds.s3-us-west-1.amazonaws.com/You+may+enter.mp4')
+video.video.attach(io: file, filename: 'You may enter.mp4')
+
+user = User.create(username: 'nigahiga', password: 'nigahiga')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "The Big Bouncing Inflatable Green Ball", description: "I made this a long time ago, completely forgot about it. P.S. I was totally gonna do the next one spoofing Oxyclean, can't do that anymore. R.I.P. Billy Mays.", user_id: 10)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/nigahiga.mp4")
+video.video.attach(io: file, filename: "nigahiga.mp4")
+
+user = User.create(username: 'Guinness World Records', password: 'nigahiga')
+user.profile_picture.attach(io: File.open(File.join(Rails.root, "app/assets/images/signin.jpeg")), filename: "default_profile_pic.jpeg")
+video = Video.create(title: "Loudest Purring Cat - Guinness World Records", description: "Yeaight", user_id: 11)
+file = open("https://yootoob-seeds.s3-us-west-1.amazonaws.com/Loudest+Purring+Cat+-+Guinness+World+Records.mp4")
+video.video.attach(io: file, filename: "cat.mp4")
--- frontend/components/video/show_video.jsx
+++ frontend/components/video/show_video.jsx
@@ -40,48 +40,98 @@ class ShowVideo extends React.Component {
         let year = date.getFullYear();
         return (
             <div className='show-body'>
-                <div id='primary' className='show-body-left'>
-                    <div>
-                        <div className='player-outer'>
-                            <div className='player-container-outer'>
-                                <div className='player-container-inner'>
-                                    <div className='player-container'>
-                                        <video className='player' controls autoPlay>
-                                            <source src={this.props.video.video} />
-                                        </video>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='info'>
-                            <div className='info-contents'>
-                                <div>
-                                    <h1 className='title-show'>
-                                        <span className='title-show-content'>
-                                            {this.props.video.title}
-                                        </span>
-                                    </h1>
-                                    <div className='below-title'>
-                                        <div className='info-text'>
-                                            <div className='count'>
-                                                <span className='count-value'>
-                                                    {this.props.video.views} views
-                                                </span>
-                                                <span>{"    "}</span>
-                                                <span className='date'>
-                                                    {month}/{day}/{year}
-                                                </span>
+                <div className='something'>
+                    <div className='columns'>
+                        <div id='primary' className='show-body-left'>
+                            <div>
+                                <div className='player-outer'>
+                                    <div className='player-container-outer'>
+                                        <div className='player-container-inner'>
+                                            <div className='player-container'>
+                                                <video className='player' controls autoPlay>
+                                                    <source src={this.props.video.video} />
+                                                </video>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div className='info'>
+                                    <div className='info-contents'>
+                                        <div>
+                                            <h1 className='title-show'>
+                                                <span className='title-show-content'>
+                                                    {this.props.video.title}
+                                                </span>
+                                            </h1>
+                                            <div className='below-title'>
+                                                <div className='info-text'>
+                                                    <div className='count'>
+                                                        <span className='count-value'>
+                                                            {this.props.video.views} views
+                                                        </span>
+                                                        <span>{"    "}</span>
+                                                        <span className='below-title-date'>
+                                                            {month}/{day}/{year}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                {/* <div className='menu-container'>
+                                                    <div className='menu'>
+                                                        <div className='menu-renderer'>
+                                                            <div className='top-level-buttons'>
+                                                                <div className='like-button'>
+                                                                    <a className='like-button-toggle'>
+                                                                        <div className='like-button-button'></div>
+
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='bottom-border-info'>
+                                    <div className='top-row'>
+                                        <div className='top-row-renderer'>
+                                            <div className='owner-icon'>
+                                                <img className='icon-margin' src={this.props.video.profilePicture} alt="profile-pic"/>
+                                            </div>
+                                            <div className='upload-info'>
+                                                <div className='uploader-name'>
+                                                    <div className='no-long-names'>
+                                                        <div className='only-block'>
+                                                            <div className='display-uploader'>
+                                                                {this.props.video.username}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='collapser collapsed'>
+                                        <div className='collapser-content'>
+                                            <div className='collapser-description'>
+                                                <div className='description-format'>
+                                                    {this.props.video.description}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button className='show-more'>
+                                            <span className='show-more-string'>Show more</span>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div id='secondary' className='show-body-right'>
-                    <div>
-                        Hi
+                        <div id='secondary' className='show-body-right'>
+                            <div>
+                                Hi
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
