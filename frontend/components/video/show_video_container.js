7e8261533e1d132d93ad2f27f8615642a9066f47
Herman <hexie646>
2020-01-17 08:03:01 -0800

    half of show

--- frontend/components/video/show_video_container.js
+++ frontend/components/video/show_video_container.js
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import ShowVideo from './show_video';
-import { fetchVideo } from '../../actions/video_actions';
+import { fetchVideo, fetchVideos } from '../../actions/video_actions';
 import { withRouter } from 'react-router-dom'
 
+
 const mapStateToProps = (state, ownProps) => {
+    // const videos = Object.values(state.entities.videos)
     return (
         {
-            video: state.entities.videos[ownProps.match.params.id]
+            video: state.entities.videos[ownProps.match.params.id],
+            // videos: videos
         }
     )
 }
@@ -14,6 +17,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => ({
     fetchVideo: (videoId) => dispatch(fetchVideo(videoId)),
+    // fetchVideos: () => dispatch((fetchVideos()))
 })
 
 export default withRouter(connect(
